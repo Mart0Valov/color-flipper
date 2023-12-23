92b7c1c38886e000a55e53a69c6d571ddecac815
Martin Valov <martin.valov98@gmail.com>
2023-12-23 19:01:31 +0200

    adding default colors for the app

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const colorDisplay = document.getElementById('color-display');
 const colorName = document.getElementById('color-name');
 const changeColorBtn = document.getElementById('change-color-btn');
 
+// setting default colors for the app
+const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
