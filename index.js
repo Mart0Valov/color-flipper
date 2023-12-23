2f5ad85ee4ba2691a98d2d16df6868b82fdc9246
Martin Valov <martin.valov98@gmail.com>
2023-12-23 18:59:09 +0200

    selecting the needed elements for the app

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+// selecting needed elements
+const colorDisplay = document.getElementById('color-display');
+const colorName = document.getElementById('color-name');
+const changeColorBtn = document.getElementById('change-color-btn');
 
